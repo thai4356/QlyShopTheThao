ada6f11c453e8e2c6195438564c3ca507c3f1f29
trinhhung12345 <wearingarmor12345@gmail.com>
2025-05-27 12:03:37 +0700

    Cập nhật xử lý đăng ký

--- src/controller/xulyDangKi.php
+++ src/controller/xulyDangKi.php
@@ -56,9 +56,9 @@ function sendVerificationEmail($email, $token) {
         $mail->isHTML(true);  //Set email format to HTML
         $mail->Subject = 'Xac thuc tai khoan';
 
-        $mail->Body = "Nhấn vào <a href='http://localhost/kiemtra2-2/src/controller/verify.php?email=$email&token=$token'>đây</a> để xác thực tài khoản.";
+        $mail->Body = "Nhấn vào <a href='http://localhost/QlyShopTheThao/src/controller/verify.php?email=$email&token=$token'>đây</a> để xác thực tài khoản.";
 
-        $mail->AltBody = 'Bam vao de xac thuc tai khoan: http://localhost/kiemtra2-1/src/view/resetpassword.php?token=' . $token;
+        $mail->AltBody = 'Bam vao de xac thuc tai khoan: http://localhost/QlyShopTheThao/src/view/resetpassword.php?token=' . $token;
 
 
         $mail->send();
